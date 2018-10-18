e4d264bb3040dc762276554cdee0a39a8798f6d9
George Coleman <colemang@ecm15-mac-14.coventry.ac.uk>
2018-10-18 09:54:51 +0100

    Added unit tests

--- listMakerTests/listMakerTests.swift
+++ listMakerTests/listMakerTests.swift
@@ -10,25 +10,35 @@ import XCTest
 @testable import listMaker
 
 class listMakerTests: XCTestCase {
-
+    
+    var lister = Lister()
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
+    
+    
+    func testAddItem() {
+        self.lister.add(item: "Bread")
+        let newItem = lister.getItem(index: 0)
+        XCTAssertEqual(newItem, "Bread")
+    }
+    
+  // func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+ //   }
 
-    func testPerformanceExample() {
+  //  func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
+    //    self.measure {
             // Put the code you want to measure the time of here.
-        }
-    }
+     //   }
+    //}
 
 }
