cafc28522419b991fe9fe2326904122bd9a222b7
George Coleman <colemang@ecm15-mac-14.coventry.ac.uk>
2018-10-18 09:10:08 +0100

    intro to swift

---
+++ listMaker/listMaker.h
@@ -0,0 +1,19 @@
+//
+//  listMaker.h
+//  listMaker
+//
+//  Created by George Coleman on 18/10/2018.
+//  Copyright © 2018 George Coleman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for listMaker.
+FOUNDATION_EXPORT double listMakerVersionNumber;
+
+//! Project version string for listMaker.
+FOUNDATION_EXPORT const unsigned char listMakerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <listMaker/PublicHeader.h>
+
+
